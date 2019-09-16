7bda9bc1b5cafe1061912a68a72fae991dad02ba
SaraPena <pena.e.sara@gmail.com>
2019-09-16 09:35:35 -0500

    Add 4.3_bonus.py & import_lesson.py

---
+++ 4.3_bonus.py
@@ -0,0 +1,10 @@
+time = "4:30pm"
+
+import import_lesson
+
+a= import_lesson.sayhello()
+print(a)
+
+
+
+print(import_lesson.t_value)
---
+++ import_lesson.py
@@ -0,0 +1,4 @@
+def sayhello():
+    return True
+
+t_value = 0.0
